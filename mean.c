087cae35cf6c24b72d22c0f6ed699e2ce2df058c
Farhad Hossain <54464172+farhadjava@users.noreply.github.com>
2024-03-20 04:27:00 +0600

    Update mean.c

--- mean.c
+++ mean.c
@@ -5,13 +5,13 @@
 void password();
 void heading();
 void mainMenu();
+
 void addbook();
 void viewbook();
-//void searchbook();
-//void editbook();
-//void deletebook();
-//void Helpbook();
-//void exit();
+void searchbook();
+void editbook();
+void deletebook();
+void exit();
 
 struct book_info{
      int id;
@@ -23,7 +23,7 @@ struct book_info{
  
 struct book_info a;
 
-FILE *file;
+FILE *file, *file2;
 
 int d = 1;
 
@@ -41,8 +41,7 @@ void mainMenu()
   printf("\t\t   3.search book\n");
   printf("\t\t   4.edit book\n");
   printf("\t\t   5.delete book\n");
-  printf("\t\t   6.Help\n");
-  //printf("\t\t   7.exit\n");
+  printf("\t\t   6.exit\n");
 
   printf("\t\t^^^^^^^^^^^^^^^^^^^^^^^\n");
   printf("\t\tEnter your choice:");
@@ -56,20 +55,18 @@ void mainMenu()
        viewbook();
    }
     else if(n==3){
-        //searchbook();
+        searchbook();
     }
      else if(n==4){ 
-         //editbook();
+         editbook();
      }
       else if(n==5){ 
-          //deletebook();
+          deletebook();
       }
+
        else if(n==6){ 
-           //Helpbook();
-        }
-        //else if(n==7){ 
-             //exit();
-         //}
+             exit (int __status);
+       }
        else{
           printf("\n\n \t\t Your choice is Wrong! \n \t\t Let's Try agani...");
           fflush(stdin);
@@ -134,17 +131,18 @@ void viewbook(){
      int count = 0;
      printf("\n\n\t\t ********** ViewBook List ***********\n\n");
      printf("\tID\tName\tAuthor\tQuantity\tRack\n\n");
-     file = fopen("books.dat","rb");
+     file = fopen("books.bat","rb");
      while(fread(&a,sizeof(a),1,file)==1){
-         printf("\t%s",a.id);
+         printf("\t%d",a.id);
          printf("\t%s",a.name);
          printf("\t%s",a.author);
          printf("\t%s",a.quantity);
          printf("\t%s",a.rack);
-
-         count = count + a.quantity;
+        
+         count = count + *(a.quantity); 
 
   }
+  
   printf("\n\t\t Have a total books %d",count);
   printf("\n\n\t\tpress any key...");
   fflush(stdin);
@@ -153,23 +151,145 @@ void viewbook(){
   mainMenu();
 }
 
-//void editbook(){
-     //printf("Can you Edit the book...!");
-//}
-//void searchbook(){
-     //printf("Now you can Searchbook...!");
-//}
+void searchbook(){
+     int d, count = 0;
+     printf("\n\n\t\t ********** search Book ********\n");
+     printf("\n\t\t searching ......\n\n");
+     file = fopen("books.dat","rb");
+     
+     printf("\n\t\t Enter ID: ");
+     scanf("%d",&d);
+     while(fread(&a,sizeof(a),1,file)==1){
+          if(d == a.id){
+              printf("\n\n\t\t Book is Found..! \n");
+              printf("\t\t ID: %d",a.id);
+              printf("\n\t\t Name: %s",a.name);
+              printf("\n\t\t Author: %s",a.author);
+              printf("\n\t\t quantity: %s",a.quantity);
+              printf("\n\t\t Rack: %s",a.rack);
+              
+              count = 1;
+          }
+     
+     }
+     if(count == 0){
+         printf("\n\n Book is not Found  !");
+     }
+     
+     printf("\n\n\t\t press any key......");
+     fflush(stdin);
+     getchar();
+     
+     mainMenu();
+     
+}
 
-//void Helpbook(){
-     //printf("From now Any book you can want to help...!");
-//}
-//void deletebook(){
-     //printf("Now as your wish can be Deletebook...!");
-//}
+void editbook(){
+     int d,count=0;
+     printf("\n\n\t\t ********* edit book ******** \n\n");
+     file = fopen("book.dat","rb+");
+     
+     printf("\n\n\t Enter your ID:\n");
+     scanf("%d",&d);
+     
+     while(fread(&a,sizeof(a),1,file)==1){
+         if(d == a.id){
+           printf("\n\t\t Book is aviabile\n");
+           printf("\n\n ID : %d",a.id);
+           printf("\n\t\t Enter New Name : ");
+           scanf("%s",a.name);
+           printf("\n\t\t Enter New Author : ");
+           scanf("%s",a.author);
+           printf("\n\t\t Enter New Quantity : ");
+           scanf("%d",&a.quantity);
+           printf("\n\t\t Enter New Rack : ");
+           scanf("%d",&a.rack);
+           
+           fseek(file,ftell(file) - sizeof(a),0);
+           
+           fwrite(&a,sizeof(a),1,file);
+           fclose(file);
+           count = 1;
+         
+         }
+     
+     }
+     if(count == 0){
+       printf("\n\t\t Book is not found :");
+     
+     }
+     printf("press any key.....");
+     fflush(stdin);
+     getchar();
+     mainMenu();
+}
 
-//void exit(){
-     //printf("Open Exit...!");
-//}
+void deletebook(){
+   int d,count = 0;
+   printf("\n\n\t\t********* Delete Book ********");
+   printf("\n\n\t\t Enter id for delete book");
+   scanf("%d",&d);
+   
+   file = fopen("book.dat","rb+");
+   rewind(file);
+   while(fread(&a,sizeof(a),1,file)==1){
+        
+        if(d == a.id){
+          printf("\n\t\t The books is availble..");
+          printf("\n\t\t Book name is:%s",a.name);
+          printf("\n\t\t Rack No:%d",a.rack);
+          
+          count = 1;     
+        }
+    }
+    
+    if(count == 0){
+      printf("\n\t\t This book is not availble");
+    }
+    else{
+        file2 = fopen("text.dat","wb");
+        
+        rewind(file);
+        while(fread(&a,sizeof(a),1,file)==1){
+            
+            if(d != a.id){
+            
+              fseek(file2,ftell(file2)-sizeof(a),0);
+              fwrite(&a,sizeof(a),1,file);
+            }
+            
+            fclose(file);
+            fclose(file2);
+            
+            remove("books.dat");
+            rename("text.dat","books.dat");
+            file= fopen("books,dat","rb");
+            
+            fclose(file);
+        }
+    }
+    printf("\n\t\t press any key....!");
+    fflush(stdin);
+    getchar();
+    
+    mainMenu();
+         
+}
+
+void exit ( int status){
+
+     int i;
+
+     printf("\n\n\t\t This is simple Library Project \n");
+     printf("\n\n\n \t\t\t Thank you \n\n");
+     printf("\n\t\t\t Wait....");
+     
+     for(i = 1; i < 1e8; i++){
+     
+     }
+     
+     exit(0);
+}
 
 // we are working take heading file.
 
